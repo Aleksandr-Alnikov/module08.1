33eedc1c8b29dddeb1e44dfb2ea2f6b8d8c48440
Alnikov Aleksandr <web3aleksandr@gmail.com>
2024-07-25 23:35:19 +0700

    ***

---
+++ index.js
@@ -0,0 +1,6 @@
+import {add, subtract} from "./src/server.js";
+
+console.log(add(10, 20));
+console.log(subtract(30, 20));
+
+console.log('Hello from Parcel!');
---
+++ src/server.js
@@ -0,0 +1,3 @@
+export const {add, subtract} = require('./math.js');
+
+
